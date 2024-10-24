742c38d65e7a8dc79ec65feb9eb15195ca9346a1
aditi <aditigaikwad0110@gmail.com>
2024-10-24 11:49:09 +0530

    basic question

---
+++ JS-Basic_question/1-10.js
@@ -0,0 +1,46 @@
+// 1-10 basic question = data-types and typeof operator
+
+// 1) Create a variable with the STRING data-type and assign the value 'HELLO WORLD!' to it;
+
+const declareString = "Hello World!.";
+console.log(declareString);
+
+// 2) Create a variable with the NUMBER data-type and assign with the value 42 to it
+
+const declareNumber = 42;
+console.log(declareNumber);
+
+// 3) Create a variable with the BOOLEAN data-type and assign the value true to it;
+
+const declareBool = true;
+console.log(declareBool);
+
+// 4) Create a variable with the value NULL;
+
+const declareNull = null;
+console.log(declareNull);
+
+// 5) Create a variable with the value Undefined;
+
+let declareUndefined;
+console.log(declareUndefined);
+
+//6) Using the TYPEOF operator, find the data type of the variable we have declare for 'HELLO WORLD!'
+
+console.log(typeof declareString);
+
+// 7) Using the TYPEOF operator, find the data type of the variable we have declare for 42;
+
+console.log(typeof declareNumber);
+
+// 8) Using the TYPEOF operator, find the data type of the variable we have declare for true;
+
+console.log(typeof declareBool);
+
+// 9) Using the TYPEOF operator, find the data type of the variable we have to declare for null
+
+console.log(typeof declareNull);
+
+// 10) Using the TYPEOF operator, find the data type of the variable we have declare for undefined
+
+console.log(typeof declareUndefined);
